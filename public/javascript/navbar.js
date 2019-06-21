fbecc406f984b99fae5f42c7d4d3f098a9a101b0
James Daniel Barrett <jamesdbarrett1992@gmail.com>
2019-06-21 11:20:31 -0700

    initial new heroku server commit

---
+++ public/javascript/navbar.js
@@ -0,0 +1,75 @@
+const hamburger = document.querySelector("#hamburger");
+const hiddenLinks = document.querySelector("#hidden-links");
+const contactButton = document.querySelector("#contact-button");
+const hiddenContact = document.querySelector("#hidden-contact");
+const navbar = document.querySelector("nav");
+const productsLinkPc = document.querySelector("#products-link-pc");
+const productsLinkMob = document.querySelector("#products-link-mob");
+const hiddenProductsPc = document.querySelector("#hidden-products-pc");
+const hiddenProductsMob = document.querySelector("#hidden-products-mob");
+
+const buttons = document.querySelectorAll("button");
+
+let hamOpen = hiddenLinks.style.display === "block";
+let contactOpen = hiddenContact.style.display === "block";
+let productsOpenPc = hiddenProductsPc.style.display === "block";
+let productsOpenMob = hiddenProductsMob.style.display === "block";
+
+
+// buttons.forEach(button => {
+//   button.addEventListener("click", event => {
+//         // button.style.width = String(button.style.width * 2);
+//         button.setAttribute("class", "enlarge");
+//   });
+// });
+
+contactButton.addEventListener("click", event => {
+   contactOpen = !contactOpen;
+   
+   if (contactOpen) {
+       hiddenContact.style.display = "block";
+   } else {
+       hiddenContact.style.display = "none";
+   }
+});
+
+productsLinkMob.addEventListener("click", event => {
+    productsOpenMob = !productsOpenMob;
+    console.log("products clicked");
+    if (productsOpenMob) {
+        hiddenProductsMob.style.display = "block";
+    } else {
+        hiddenProductsMob.style.display = "none";
+    }
+});
+
+productsLinkPc.addEventListener("click", event => {
+    productsOpenPc = !productsOpenPc;
+    console.log("products clicked");
+    if (productsOpenPc) {
+        hiddenProductsPc.style.display = "block";
+    } else {
+        hiddenProductsPc.style.display = "none";
+    }
+});
+
+hamburger.addEventListener("click", event => {
+   hamOpen = !hamOpen;
+   
+   if (hamOpen) {
+        hiddenLinks.style.display = "block";
+   } else {
+        hiddenLinks.style.display = "none";
+   }
+});
+
+window.addEventListener("scroll", event => {
+    if (window.scrollY > 20) {
+        navbar.style.position = "fixed";
+        navbar.style.boxShadow = "0px 5px 10px black";
+    } else {
+        navbar.style.position = "relative";
+        navbar.style.boxShadow = "0 0 0 white";
+        hiddenLinks.style.boxShadow = "0 0 0 white"; 
+    }
+});
---
+++ public/javascript/showcase.js
@@ -0,0 +1,24 @@
+let mainImage = document.querySelector("#main-image");
+let thumbnails = document.querySelectorAll(".thumbnail");
+
+
+thumbnails.forEach(thumbnail => {
+   thumbnail.addEventListener("click", () => {
+       mainImage.setAttribute("src", thumbnail.getAttribute("src"));
+       changeFocus();
+       console.log("thumbnail clicked");
+   });
+});
+
+function changeFocus() {
+    thumbnails.forEach(thumbnail => {
+       if (thumbnail.getAttribute("src") === mainImage.getAttribute("src")) {
+           thumbnail.style.opacity = "0.7";
+       }  else {
+           thumbnail.style.opacity = "1";
+       }
+    });
+}
+
+
+changeFocus();
